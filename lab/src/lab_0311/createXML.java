ac0d1dce7f62a5c73d05d1936149ec3d26255bec
shinhaeyoung <godud208@gmail.com>
2021-03-17 16:00:15 +0900

    lab_0318: lab_0311 commit

---
+++ lab/src/lab_0311/createXML.java
@@ -0,0 +1,97 @@
+package lab_0311;
+
+import java.io.*;
+
+import javax.xml.parsers.*;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import org.w3c.dom.Element;
+
+public class createXML {
+
+	public static void main(String[] args) throws IOException, ParserConfigurationException {
+		String path = "../"; //C:/201811191 신해영/2021 4학년 1학기/오픈소스SW입문/SimpleIR/
+		
+		//.html 파일 접근
+		File files = new File(path);
+		String pattern = ".html";
+		
+		String fileList[] = files.list(new FilenameFilter() {
+			@Override
+			public boolean accept(File dir, String name) {
+				return name.endsWith(pattern); //.html 형식으로 끝나는 파일
+			}
+		});
+		
+		//xml 파일
+		DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+		DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+		
+		org.w3c.dom.Document doc = docBuilder.newDocument();
+		
+		//docs element
+		Element docs = doc.createElement("docs");
+		doc.appendChild(docs);
+		
+		//xml 생성
+		if(fileList.length > 0) {
+			for(int i=0; i<fileList.length; i++) {
+//				System.out.println(fileList[i]);
+				
+				//doc element
+				Element ele_doc = doc.createElement("doc");
+				docs.appendChild(ele_doc);
+				//doc id 속성
+				ele_doc.setAttribute("id", Integer.toString(i));
+				
+				//parsing
+				Document document = Jsoup.parse(new File(path + "/" + fileList[i]), "UTF-8");
+				
+				//title element
+				Element title = doc.createElement("title");
+				//html의 title->xml의 title
+				Elements titles = document.select("title");
+//				System.out.println("title: " + titles.html());
+				title.appendChild(doc.createTextNode(titles.html()));
+				ele_doc.appendChild(title);
+				
+				//body element
+				Element body = doc.createElement("body");
+				//html의 p->xml의 body
+				Elements ps = document.select("#content p");
+				for(org.jsoup.nodes.Element e: ps)
+//					System.out.println(e.html());
+					body.appendChild(doc.createTextNode(e.html()));
+				ele_doc.appendChild(body);
+			}
+		}
+		
+		//XML 파일로 쓰기
+		TransformerFactory transformerFactory = TransformerFactory.newInstance();
+		
+		Transformer transformer = null;
+		try {
+			transformer = transformerFactory.newTransformer();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		}
+		transformer.setOutputProperty(OutputKeys.ENCODING, "UTF-8");
+		
+		DOMSource source = new DOMSource(doc);
+		StreamResult result = new StreamResult(new FileOutputStream(new File("../collection.xml")));
+		
+		try {
+			transformer.transform(source, result);
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
